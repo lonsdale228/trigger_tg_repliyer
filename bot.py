a21d92436760b28be34492908ec7f2a213ff77e1
lonsdale228 <v.gerashchenkoua@gmail.com>
2025-07-19 19:35:42 +0300

    Added db implementation

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import os
 
+from db.connection import sessionmanager
+from db.models import Base
+
 
 async def main():
     print("123")
+    Base.metadata.create_all(sessionmanager._engine)
 
 
 if __name__ == "__main__":
--- db/models.py
+++ db/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import BigInteger, Boolean, DateTime, String, ForeignKey
+from sqlalchemy import BigInteger, Boolean, String, ForeignKey, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-class Base(DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     pass
 
@@ -13,9 +14,11 @@ class Users(Base):
     __tablename__ = "users"
 
     user_tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
-    is_admin: Mapped[bool] = mapped_column(Boolean)
-    register_date: Mapped[datetime] = mapped_column(DateTime)
-    is_banned: Mapped[bool] = mapped_column(Boolean)
+    is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    register_date: Mapped[datetime] = mapped_column(
+        server_default=func.now(), nullable=False
+    )
+    is_banned: Mapped[bool] = mapped_column(Boolean, nullable=False)
     user_details: Mapped["UserDetails"] = relationship(
         "UserDetails", back_populates="user"
     )
@@ -27,7 +30,7 @@ class UserDetails(Base):
     user_tg_id: Mapped[int] = mapped_column(
         BigInteger, ForeignKey("users.user_tg_id"), unique=True, nullable=False
     )
-    username: Mapped[str] = mapped_column(String, nullable=True)
-    fullname: Mapped[str] = mapped_column(String, nullable=True)
+    username: Mapped[str | None] = mapped_column(String, nullable=True)
+    fullname: Mapped[str | None] = mapped_column(String, nullable=True)
 
     user: Mapped["Users"] = relationship("Users", back_populates="user_details")
